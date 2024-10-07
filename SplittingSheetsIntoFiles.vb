ebdba38e994d6c21c7a890c90e1190fc5cf03fdf
Luka Ćurić <146451386+LukaCuric0410@users.noreply.github.com>
2024-10-07 11:04:27 +0200

    Update SplittingSheetsIntoFiles.vb

--- SplittingSheetsIntoFiles.vb
+++ SplittingSheetsIntoFiles.vb
@@ -1,4 +1,4 @@
-Sub ExportSheetsToCSV()
+Sub ExportSheetsToCSVIfSpecificTextFound()
     Dim ws As Worksheet
     Dim FilePath As String
     Dim FileName As String
@@ -7,11 +7,24 @@ Sub ExportSheetsToCSV()
     Dim RowNum As Long
     Dim ColNum As Long
     Dim Line As String
+    Dim CheckCellContent As String
+    Dim SpecificText As String
+    Dim LastRow As Long
+    Dim TextFound As Boolean
     
-    ' Postavite putanju gdje će se spremiti CSV datoteke
-    FilePath = "/Users/CuricL/Desktop/AnamariaTest/"
+    ' Prikazuje InputBox za unos specifi?nog teksta
+    SpecificText = InputBox("Unesite tekst koji zelite traziti u stupcu A:", "Unos trazenog teksta")
     
-    ' Provjera i dodavanje završnog backslasha ako nedostaje
+    ' Ako korisnik ne unese ni?ta, iza?i iz procedure
+    If SpecificText = "" Then
+        MsgBox "Niste unijeli tekst.", vbExclamation
+        Exit Sub
+    End If
+    
+    ' Postavite putanju gdje ?e se spremiti CSV datoteke
+    FilePath = "/Users/CuricL/Desktop"
+    
+    ' Provjera i dodavanje zavr?nog backslasha ako nedostaje
     If Right(FilePath, 1) <> "/" Then
         FilePath = FilePath & "/"
     End If
@@ -20,39 +33,60 @@ Sub ExportSheetsToCSV()
     If Dir(FilePath, vbDirectory) = "" Then
         MsgBox "Putanja ne postoji! Molimo unesite ispravnu putanju.", vbExclamation
         Exit Sub
+    Else
+        MsgBox "Putanja je prepoznata: " & FilePath, vbInformation
     End If
 
     ' Petlja kroz svaki tab (worksheet) u aktivnoj radnoj knjizi
     For Each ws In ThisWorkbook.Worksheets
-        ' Osiguraj ispravno ime fajla (bez neispravnih znakova za naziv fajla)
-        ValidFileName = Replace(ws.Name, "/", "_")
-        ValidFileName = Replace(ValidFileName, "\", "_")
-        ValidFileName = Replace(ValidFileName, ":", "_")
-        ValidFileName = Replace(ValidFileName, "?", "_")
-        ValidFileName = Replace(ValidFileName, "*", "_")
-        ValidFileName = Replace(ValidFileName, "[", "_")
-        ValidFileName = Replace(ValidFileName, "]", "_")
+        TextFound = False ' Resetiraj oznaku za svaki radni list
         
-        ' Kreiraj puni naziv datoteke
-        FileName = FilePath & ValidFileName & ".csv"
+        ' Odredi posljednji kori?teni redak u stupcu A
+        LastRow = ws.Cells(ws.Rows.Count, 1).End(xlUp).Row
         
-        ' Otvori tekstualni fajl za pisanje
-        FileNum = FreeFile
-        Open FileName For Output As FileNum
-        
-        ' Petlja kroz svaki redak i stupac kako bi zapisao podatke u CSV
-        For RowNum = 1 To ws.UsedRange.Rows.Count
-            Line = ""
-            For ColNum = 1 To ws.UsedRange.Columns.Count
-                ' Dodaj podatke iz svake ćelije
-                Line = Line & IIf(ColNum > 1, ",", "") & ws.Cells(RowNum, ColNum).Text
-            Next ColNum
-            ' Zapiši redak u CSV fajl
-            Print #FileNum, Line
+        ' Petlja kroz sve ?elije u stupcu A (od reda 1 do LastRow)
+        For RowNum = 1 To LastRow
+            CheckCellContent = ws.Cells(RowNum, 1).Value ' ?itanje vrijednosti iz stupca A
+            
+            ' Provjera ima li ?elija specifi?an tekst
+            If CheckCellContent = SpecificText Then
+                TextFound = True ' Prona?en specifi?an tekst
+                Exit For ' Nema potrebe za daljnjom provjerom, mo?emo iza?i iz petlje
+            End If
         Next RowNum
         
-        ' Zatvori fajl
-        Close FileNum
+        ' Ako je prona?en specifi?an tekst u stupcu A, kreiraj CSV datoteku
+        If TextFound Then
+            ' Osiguraj ispravno ime fajla (bez neispravnih znakova za naziv fajla)
+            ValidFileName = Replace(ws.Name, "/", "_")
+            ValidFileName = Replace(ValidFileName, "\", "_")
+            ValidFileName = Replace(ValidFileName, ":", "_")
+            ValidFileName = Replace(ValidFileName, "?", "_")
+            ValidFileName = Replace(ValidFileName, "*", "_")
+            ValidFileName = Replace(ValidFileName, "[", "_")
+            ValidFileName = Replace(ValidFileName, "]", "_")
+            
+            ' Kreiraj puni naziv datoteke
+            FileName = FilePath & ValidFileName & ".csv"
+            
+            ' Otvori tekstualni fajl za pisanje
+            FileNum = FreeFile
+            Open FileName For Output As FileNum
+            
+            ' Petlja kroz svaki redak i stupac kako bi zapisao podatke u CSV
+            For RowNum = 1 To ws.UsedRange.Rows.Count
+                Line = ""
+                For ColNum = 1 To ws.UsedRange.Columns.Count
+                    ' Dodaj podatke iz svake ?elije
+                    Line = Line & IIf(ColNum > 1, ",", "") & ws.Cells(RowNum, ColNum).Text
+                Next ColNum
+                ' Zapi?i redak u CSV fajl
+                Print #FileNum, Line
+            Next RowNum
+            
+            ' Zatvori fajl
+            Close FileNum
+        End If
     Next ws
     
     MsgBox "I dalje ti Luka sve omogucuje!", vbInformation
